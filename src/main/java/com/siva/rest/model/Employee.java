ac637629a1f4c9625e15bc18f445268cc15718d3
Sivasai <sai612498@gmail.com>
2026-06-15 12:15:10 +0530

    Fix Employee getters setters

--- src/main/java/com/siva/rest/model/Employee.java
+++ src/main/java/com/siva/rest/model/Employee.java
@@ -22,7 +22,21 @@ public class Employee {
     private String email;
     private String department;
     private double salary;
+public Long getId() {
+    return id;
+}
 
+public void setId(Long id) {
+    this.id = id;
+}
+
+public double getSalary() {
+    return salary;
+}
+
+public void setSalary(double salary) {
+    this.salary = salary;
+}
     
    
 }
