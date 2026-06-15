ac3269be9c1e9b4df57cfb93e93bc56c9b822ad5
Sivasai <sai612498@gmail.com>
2026-06-15 12:24:58 +0530

    Fixed Employee model and removed duplicate controller

--- src/main/java/com/siva/rest/model/Employee.java
+++ src/main/java/com/siva/rest/model/Employee.java
@@ -4,14 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Entity   
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
 public class Employee {
 
     @Id
@@ -22,21 +16,55 @@ public class Employee {
     private String email;
     private String department;
     private double salary;
-public Long getId() {
-    return id;
-}
 
-public void setId(Long id) {
-    this.id = id;
-}
+    public Employee() {
+    }
 
-public double getSalary() {
-    return salary;
-}
+    public Employee(Long id, String name, String email, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.salary = salary;
+    }
 
-public void setSalary(double salary) {
-    this.salary = salary;
-}
-    
-   
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
